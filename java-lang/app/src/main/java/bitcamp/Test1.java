307c1feee372ed580d25818b94afb69744eb835a
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-05-24 17:24:11 +0900

    :package: add exam files in java-lang

---
+++ java-lang/app/src/main/java/bitcamp/Test1.java
@@ -0,0 +1,10 @@
+package bitcamp;
+
+public class Test1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+// javac src/main/java/bitcamp/Test1.java : compile
+// java -cp src/main/java bitcamp.Test1  : execute

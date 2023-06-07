ff8f3e8bfe036ac1f725763bb335493d2731faa7
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-06-07 17:50:49 +0900

    edit : calculator 수정

--- myapp/app/src/main/java/bitcamp/util/Calculator.java
+++ myapp/app/src/main/java/bitcamp/util/Calculator.java
@@ -3,8 +3,8 @@ package bitcamp.util;
 public class Calculator {
   public static int result; // static 변수는 자동적으로 0으로 초기화
 
-  public static void set(int result) {
-    result = result;
+  public static void set(int a) {
+    result = a;
   }
 
   public static void plus(int a) {

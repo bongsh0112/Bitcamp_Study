cc2045dcf8e596496a58ab644d2d436c69b66ac3
bongsh0112 <bongsh0112@gmail.com>
2023-06-21 16:54:31 +0900

    수업 내용 업데이트

--- myapp/MemberHandler.java
+++ myapp/MemberHandler.java
@@ -1,55 +1,49 @@
 package bitcamp.myapp.handler;
 
-import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.ArrayList;
 import bitcamp.util.List;
-import bitcamp.util.Prompt;
+import bitcamp.util.MenuPrompt;
 
 public class MemberHandler implements Handler {
 
   private List list;
-  private Prompt prompt;
+  private MenuPrompt prompt;
   private String title;
 
-  public MemberHandler(Prompt prompt, String title, List list) {
+  public MemberHandler(MenuPrompt prompt, String title, List list) {
     this.prompt = prompt;
     this.title = title;
     this.list = list;
   }
 
   public void execute() {
-    printMenu();
+
+    prompt.appendBreadcrumb(this.title, getMenu());
+
+    prompt.printMenu();
 
     while (true) {
-      String menuNo = prompt.inputString("%s> ", this.title);
-      if (menuNo.equals("0")) {
-        return;
-      } else if (menuNo.equals("menu")) {
-        printMenu();
-      } else if (menuNo.equals("1")) {
-        this.inputMember();
-      } else if (menuNo.equals("2")) {
-        this.printMembers();
-      } else if (menuNo.equals("3")) {
-        this.viewMember();
-      } else if (menuNo.equals("4")) {
-        this.updateMember();
-      } else if (menuNo.equals("5")) {
-        this.deleteMember();
-      } else {
-        System.out.println("메뉴 번호가 옳지 않습니다!");
+      String menuNo = prompt.inputMenu();
+      switch (menuNo) {
+        case "0": prompt.removeBreadcrumb(); return;
+        case "1": this.inputMember(); break;
+        case "2": this.printMembers(); break;
+        case "3": this.viewMember(); break;
+        case "4": this.updateMember(); break;
+        case "5": this.deleteMember(); break;
       }
     }
   }
 
-  private static void printMenu() {
-    System.out.println("1. 등록");
-    System.out.println("2. 목록");
-    System.out.println("3. 조회");
-    System.out.println("4. 변경");
-    System.out.println("5. 삭제");
-    System.out.println("0. 메인");
+  private static String getMenu() {
+    StringBuilder menu = new StringBuilder();
+    menu.append("1. 등록\n");
+    menu.append("2. 목록\n");
+    menu.append("3. 조회\n");
+    menu.append("4. 변경\n");
+    menu.append("5. 삭제\n");
+    menu.append("0. 메인\n");
+    return menu.toString();
   }
 
   private void inputMember() {
@@ -67,9 +61,8 @@ public class MemberHandler implements Handler {
     System.out.println("번호, 이름, 이메일, 성별");
     System.out.println("---------------------------------------");
 
-    Object[] arr = this.list.toArray();
-    for (Object obj : arr) {
-      Member m = (Member) obj;
+    for (int i = 0; i < this.list.size(); i++) {
+      Member m = (Member) this.list.get(i);
       System.out.printf("%d, %s, %s, %s\n",
           m.getNo(), m.getName(), m.getEmail(),
           toGenderString(m.getGender()));
@@ -79,7 +72,7 @@ public class MemberHandler implements Handler {
   private void viewMember() {
     int memberNo = this.prompt.inputInt("번호? ");
 
-    Member m = (Member) this.findBy(memberNo);
+    Member m = this.findBy(memberNo);
     if (m == null) {
       System.out.println("해당 번호의 회원이 없습니다!");
       return;
@@ -97,7 +90,7 @@ public class MemberHandler implements Handler {
   private void updateMember() {
     int memberNo = this.prompt.inputInt("번호? ");
 
-    Member m = (Member) this.findBy(memberNo);
+    Member m = this.findBy(memberNo);
     if (m == null) {
       System.out.println("해당 번호의 회원이 없습니다!");
       return;
@@ -141,13 +134,13 @@ public class MemberHandler implements Handler {
   }
 
   private Member findBy(int no) {
-    Object[] arr = this.list.toArray();
-    for (Object obj : arr) {
-      Member m = (Member) obj;
+    for (int i = 0; i < this.list.size(); i++) {
+      Member m = (Member) this.list.get(i);
       if (m.getNo() == no) {
         return m;
       }
     }
     return null;
   }
+
 }
--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,56 +1,62 @@
 package bitcamp.myapp;
 
-import bitcamp.myapp.handler.BoardHandler;
-import bitcamp.myapp.handler.MemberHandler;
-import bitcamp.util.LinkedList;
+import bitcamp.myapp.handler.BoardAddListener;
+import bitcamp.myapp.handler.BoardDeleteListener;
+import bitcamp.myapp.handler.BoardDetailListener;
+import bitcamp.myapp.handler.BoardListListener;
+import bitcamp.myapp.handler.BoardUpdateListener;
+import bitcamp.myapp.handler.MemberAddListener;
+import bitcamp.myapp.handler.MemberDeleteListener;
+import bitcamp.myapp.handler.MemberDetailListener;
+import bitcamp.myapp.handler.MemberListListener;
+import bitcamp.myapp.handler.MemberUpdateListener;
 import bitcamp.util.ArrayList;
-import bitcamp.util.List;
-import bitcamp.util.Prompt;
-
+import bitcamp.util.BreadcrumbPrompt;
+import bitcamp.util.LinkedList;
+import bitcamp.util.Menu;
+import bitcamp.util.MenuGroup;
 
 public class App {
 
   public static void main(String[] args) {
 
-    // 기본 생성자를 이용해 Prompt 인스턴스를 준비한다.
-    // => 기본 생성자는 Scanner를 키보드와 연결한다. OK
-    Prompt prompt = new Prompt();
-    List list = new LinkedList();
+    ArrayList memberList = new ArrayList();
+    LinkedList boardList = new LinkedList();
+    LinkedList readingList = new LinkedList();
 
-    MemberHandler memberHandler = new MemberHandler(prompt, "일반회원", new ArrayList());
-    BoardHandler boardHandler = new BoardHandler(prompt, "게시글", list);
-    BoardHandler readingHandler = new BoardHandler(prompt, "독서록", list);
+    BreadcrumbPrompt prompt = new BreadcrumbPrompt();
 
+    MenuGroup mainMenu = new MenuGroup("메인");
+
+    MenuGroup memberMenu = new MenuGroup("회원");
+    memberMenu.add(new Menu("등록", new MemberAddListener(memberList)));
+    memberMenu.add(new Menu("목록", new MemberListListener(memberList)));
+    memberMenu.add(new Menu("조회", new MemberDetailListener(memberList)));
+    memberMenu.add(new Menu("변경", new MemberUpdateListener(memberList)));
+    memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberList)));
+    mainMenu.add(memberMenu);
+
+    MenuGroup boardMenu = new MenuGroup("게시글");
+    boardMenu.add(new Menu("등록", new BoardAddListener(boardList)));
+    boardMenu.add(new Menu("목록", new BoardListListener(boardList)));
+    boardMenu.add(new Menu("조회", new BoardDetailListener(boardList)));
+    boardMenu.add(new Menu("변경", new BoardUpdateListener(boardList)));
+    boardMenu.add(new Menu("삭제", new BoardDeleteListener(boardList)));
+    mainMenu.add(boardMenu);
+
+    MenuGroup readingMenu = new MenuGroup("독서록");
+    readingMenu.add(new Menu("등록", new BoardAddListener(readingList)));
+    readingMenu.add(new Menu("목록", new BoardListListener(readingList)));
+    readingMenu.add(new Menu("조회", new BoardDetailListener(readingList)));
+    readingMenu.add(new Menu("변경", new BoardUpdateListener(readingList)));
+    readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingList)));
+    mainMenu.add(readingMenu);
 
     printTitle();
 
-    printMenu();
-
-    while (true) {
-      String menuNo = prompt.inputString("메인> ");
-      if (menuNo.equals("0")) {
-        break;
-      } else if (menuNo.equals("menu")) {
-        printMenu();
-      } else if (menuNo.equals("1")) {
-        memberHandler.execute();
-      } else if (menuNo.equals("2")) {
-         boardHandler.execute();
-      } else if (menuNo.equals("3")) {
-         readingHandler.execute();
-      }  else {
-        System.out.println("메뉴 번호가 옳지 않습니다!");
-      }
-    }
+    mainMenu.execute(prompt);
 
     prompt.close();
-  }
-
-  static void printMenu() {
-    System.out.println("1. 회원");
-    System.out.println("2. 게시글");
-    System.out.println("3. 독서록");
-    System.out.println("0. 종료");
   }
 
   static void printTitle() {
